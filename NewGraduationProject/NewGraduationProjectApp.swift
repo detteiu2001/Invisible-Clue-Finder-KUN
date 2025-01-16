79db97bac5cae901f7c583779f97f8cc63df3388
detteiu2001 <23cm0128@jec.ac.jp>
2025-01-16 09:32:25 +0900

    Initial Commit

---
+++ NewGraduationProject/NewGraduationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewGraduationProjectApp.swift
+//  NewGraduationProject
+//
+//  Created by cmStudent on 2025/01/16.
+//
+
+import SwiftUI
+
+@main
+struct NewGraduationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
